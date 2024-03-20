491cbccdd47b8511930e7bb6f5651943d2cf9e38
mukamuka38 <mukamuka38@yahoo.co.jp>
2024-03-20 17:05:15 +0900

    zinit と zsh プラグインをを追加
    以下の３点を追加
        ・_zsgrc に zinit プラグイン環境を追加
        ・powerlevel3k、fast-syntax-highlighting、zsh-autosuggestions、git-open、bat プラグインを追加
        ・zsh/config.zsh に peco設定 のコマンド履歴検索、ディレクトリ移動（コマンド履歴）、ディレクトリ移動
        （カレントディレクトリ以下のディレクトリ）、Gitリポジトリディレクトリに移動を追加# This is the commit message #2:

---
+++ zsh/config.zsh
@@ -0,0 +1,51 @@
+# zsh/config.zsh
+
+## コマンド履歴検索
+function peco-history-selection() {
+  BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+  CURSOR=$#BUFFER
+  zle reset-prompt
+}
+zle -N peco-history-selection
+bindkey '^R' peco-history-selection
+
+## コマンド履歴からディレクトリ検索・移動
+if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]]; then
+  autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+  add-zsh-hook chpwd chpwd_recent_dirs
+  zstyle ':completion:*' recent-dirs-insert both
+  zstyle ':chpwd:*' recent-dirs-default true
+  zstyle ':chpwd:*' recent-dirs-max 1000
+  zstyle ':chpwd:*' recent-dirs-file "$HOME/.cache/chpwd-recent-dirs"
+fi
+function peco-cdr () {
+  local selected_dir="$(cdr -l | sed 's/^[0-9]* *//' | peco)"
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+}
+zle -N peco-cdr
+bindkey '^E' peco-cdr
+
+## カレントディレクトリ以下のディレクトリ検索・移動
+function find_cd() {
+  local selected_dir=$(find . -type d | peco)
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+}
+zle -N find_cd
+bindkey '^X' find_cd
+
+## gitリポジトリ検索・移動
+function peco-src () {
+  local selected_dir=$(ghq list -p | peco)
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+}
+zle -N peco-src
+bindkey '^G' peco-src
